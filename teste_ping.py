bdf134c5a44b2d1d009e122aafa61a9a83808498
Matheus Fantoni <55607715+matheusfantoni@users.noreply.github.com>
2022-06-09 20:05:06 -0300

    Add files via upload

---
+++ teste_ping.py
@@ -0,0 +1,22 @@
+import socket
+
+arquivo = open('hosts.txt', 'r')
+saida = open('saida.txt', 'w')
+
+hosts = arquivo.readlines()
+i = 0
+
+while i < 579:  ## Definir o número de hosts do teste
+  try: 
+    ip = socket.gethostbyname(hosts[i].rstrip())
+  except:
+    saida.writelines(hosts[i].rstrip("\n") + " " + "ERRO NO TESTE DE PING")
+    saida.writelines("\n")
+  finally:
+    print(ip)
+    saida.writelines(hosts[i].rstrip("\n") + ";" + ip)
+    saida.writelines("\n")
+    i += 1
+
+arquivo.close() 
+saida.close()
